867594b2868295590020979574aca8f319b5c30a
ZRiemann <25452483@qq.com>
2016-10-09 19:25:48 +0800

    update net/socket module

--- src/socket.c
+++ src/socket.c
@@ -18,8 +18,22 @@ zsock_t zsocket(int domain, int type, int protocol){
 #if ZTRACE_SOCKET
     ZDBG("socket<%d>(domain<%d>,type<%d>,protocol<%d>", sock, domain, type, protocol);
 #endif
+    zsock_noblock(sock, ZTRUE); // set default no block
   }
   return(sock);
+}
+
+int zsockclose(zsock_t sock){
+  int ret;
+#ifdef ZSYS_WINDOWS
+  ret = closesocket(sock);
+  ret = (SOCKET_ERROR == ret) ? WSAGetLastError() : ZOK;
+#else //ZSYS_POSIX
+  ret = close(sock);
+  ret = (ret < 0)?errno:ZOK;
+#endif
+  ZERRC(ret);
+  return(ret);
 }
 
 int zinet_addr(zsockaddr_in *addr, const char *host, uint16_t port){
@@ -30,7 +44,6 @@ int zinet_addr(zsockaddr_in *addr, const char *host, uint16_t port){
   memset(addr, 0, sizeof(zsockaddr_in));
   addr->sin_family = AF_INET;
   addr->sin_port = htons(port);
-
 #ifdef ZSYS_WINDOWS
   if(INADDR_NONE == (addr->sin_addr.S_un.S_addr = inet_addr(host))){
     ret = ZFUN_FAIL;
@@ -66,7 +79,8 @@ int zconnect(zsock_t sock, const ZSA *addr, int len){
 
 int zrecv(zsock_t sock, char *buf, int len, int flags){
   int ret;
-  if(0 > (recv(sock, buf, len, flags))){
+  int readed;
+  if(0 > (readed = recv(sock, buf, len, flags))){
 #ifdef ZSYS_WINDOWS
     ret = WSAGetLastError();
     if(WSAEWOULDBLOCK == ret || WSAEINTR == ret){
@@ -80,9 +94,12 @@ int zrecv(zsock_t sock, char *buf, int len, int flags){
 #endif
     if(ret != ZAGAIN){
       ZERRC(ret);
+      ret = ZFUN_FAIL;
     }
+  }else{
+    ret = readed;
   }
-  return ret;
+  return(ret);
 }
 
 int zsend(zsock_t sock, const char *buf, int len, int flags){
@@ -115,25 +132,148 @@ int zsend(zsock_t sock, const char *buf, int len, int flags){
       }
     }
   }
+  if(ret != ZFUN_FAIL){
+    ret = sended;
+  }
   return(ret);
 }
 
-static int packet_bitmask(char *buf, int len, char bitmask, char *stuf){
-  if(*buf == bitmask){
-    return(ZTRUE);
-  }else{
-    return(ZTRUE);
-  }
-}
-int recv_packet(zsock_t sock, char *buf, int maxlen, int *offset, int *len, char bitmask, char *stuf){
+int zrecv_packet(zsock_t sock, char *buf, int maxlen, int *offset, int *len, char bitmask){
+  int ret = ZFAIL;
   // ZASSERT(!buf || !offset || !len || !bitmask || !stuf);
-  if(*offset != *len){
-    memmove(buf, buf + *offset, *len - *offset);
-    *offset = *len - *offset;
-    *len = *offset;
-  }else{
-    *offset = *len = 0;
+  // remove old data and move new data to header
+  //  zdbg("init offset<%d> len<%d>", *offset, *len);
+  if(*offset != 0){
+    if(*offset != *len){
+      while(*(buf+(*offset)) != bitmask){
+	//ZDBG("drop<%02x>", *(buf+(*offset)));
+	++(*offset);
+	if(*offset == *len){
+	  *offset = *len = 0;
+	  break;
+	}
+      }
+      memmove(buf, buf + *offset, *len - *offset);
+      *len = *len - *offset;
+      *offset = 0;
+      //------------------------------------
+      // parse next packet
+      if(*len != *offset){
+	//	zdbg("fefore offset<%d> len<%d>", *offset, *len);
+	while(++(*offset)){
+	  if(*offset == *len){
+	    *offset = *len = 0;
+	    break;
+	  }
+	  if(*(buf+(*offset)) == bitmask){
+	    ++(*offset);
+	    ret = *offset;
+	    //  zdbg("next offset<%d> len<%d>", *offset, *len);
+	    return(ret);
+	  }
+	}
+      }
+    }else{
+      *offset = *len = 0;
+      // zdbg("reset offset<%d> len<%d>", *offset, *len);
+    }
   }
-  packet_bitmask(NULL,0,0,NULL);
+  // start recv data
+  ret = zrecv(sock, buf+(*len), maxlen-(*len), 0);
+  if(ZAGAIN != ret && ZFUN_FAIL != ret && 0 != ret){
+    *len += ret;
+    while(++(*offset)){
+      if(*(buf+(*offset)) == bitmask){
+	++(*offset);
+	ret = *offset;
+      	//zdbg("111 offset<%d> len<%d>", *offset, *len);
+	break;
+      }
+      if(*len == *offset){
+	ret = ZAGAIN;
+      	//zdbg("222 offset<%d> len<%d>", *offset, *len);
+	break;
+      }
+    }
+    //zdbg("333 offset<%d> len<%d>", *offset, *len);
+  }
+  //zdbg("end offset<%d> len<%d>", *offset, *len);
+  return(ret);
+}
+
+int zselect(int maxfdp1, fd_set *read, fd_set *write, fd_set *except, struct timeval *timeout){
+  int ret;
+  ret = select(maxfdp1, read, write, except, timeout);
+  if(ret < 0){
+#ifdef ZSYS_WINDOWS
+    ret = WSAGetLastError();
+#else
+    ret = errno;
+#endif
+    ZERRC(ret);
+    ret = ZFUN_FAIL;
+  }
+  return(ret);
+}
+
+int zsock_noblock(zsock_t sock, int noblock){
+  //...
   return(ZOK);
+}
+
+int zbind(zsock_t sock, const ZSA *addr, int len){
+  int ret;
+  ret = bind(sock, addr, len);
+#ifdef ZSYS_WINDOWS
+  ret = (SOCKET_ERROR == ret) ? WSAGetLastError() : ZOK;
+#else
+  ret = (ret < 0) ? errno : ZOK;
+#endif
+  ZERRC(ret);
+  return(ret);
+}
+
+int zlisten(zsock_t sock, int listenq){
+  int ret;
+  ret = listen(sock, listenq);
+#ifdef ZSYS_WINDOWS
+  ret = (SOCKET_ERROR == ret) ? WSAGetLastError() : ZOK;
+#else
+  ret = (ret < 0) ? errno : ZOK;
+#endif
+  ZERRC(ret);
+  return(ret);
+}
+
+zsock_t zaccept(zsock_t sock, ZSA *addr, int *addrlen){
+  int ret;
+  zsock_t sk;
+#ifdef ZSYS_WINDOWS
+  sk = accept(sock, addr, addrlen);
+  ret = (INVALID_SOCKET == sk) ? WSAGetLastError() : ZOK;
+#else
+  sk = accept(sock, addr, (socklen_t*)addrlen);
+  ret = (sk < 0) ? errno : ZOK;
+#endif
+  ZERRC(ret);
+  return(sk);
+}
+
+int zconnectx(zsock_t sock, const char *host, uint16_t port, int listenq){
+  int ret;
+  zsockaddr_in addr;
+
+  ret = zinet_addr(&addr, host, port);
+  if(ret != ZOK){
+    return(ret);
+  }
+
+  if(listenq > 0){
+    zbind(sock, (ZSA*)&addr, sizeof(addr));
+    ret = zlisten(sock, listenq);
+  }else{
+    ret = zconnect(sock, (ZSA*)&addr, sizeof(addr));
+  }
+  ZERRC(ret);
+  return(ret);
 }
--- zit/net/socket.h
+++ zit/net/socket.h
@@ -23,6 +23,7 @@ typedef struct sockaddr ZSA;
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 
 typedef int zsock_t;
 typedef struct sockaddr_in zsockaddr_in;
@@ -38,14 +39,35 @@ typedef struct sockaddr ZSA;
  * @param int protocol [in] default 0
  */
 ZAPI zsock_t zsocket(int domain, int type, int protocol);
+ZAPI int zsockclose(zsock_t sock);
+// active connect
 ZAPI int zinet_addr(zsockaddr_in *addr, const char *host, uint16_t port);
 ZAPI int zconnect(zsock_t sock, const ZSA *addr, int len);
 ZAPI int zrecv(zsock_t sock, char *buf, int len, int flags);
 ZAPI int zsend(zsock_t sock, const char *buf, int len, int flags);
-
-/**@fn int recv_packet(sock_t sock, char *buf, int maxlen, int* offset, int *len, char *bitmask, char *stuf)
+// passive connect
+ZAPI int zbind(zsock_t sock, const ZSA *addr, int len);
+ZAPI int zlisten(zsock_t sock, int listenq); // listenq=1024
+ZAPI zsock_t zaccept(zsock_t sock, ZSA *addr, int *addrlen);
+// select() -1 infinit 0 no wait >0 wait ms 
+ZAPI int zselect(int maxfdp1, fd_set *read, fd_set *write, fd_set *except, struct timeval *timeout);
+// get/setoption()
+ZAPI int zsock_noblock(zsock_t sock, int noblock);
+/**@fn int recv_packet(sock_t sock, char *buf, int maxlen, int* offset, int *len, char *bitmask)
  * @brief recv a packet
+ * @return ZOK - sock closed
+ * @return ZAGAIN - recv ok but need more
+ * @return ZFUN_FAIL - call system API failed
+ * @return ret>0 - packet len
+ * @note
+ * buf - packet begin
+ * offset - packet end + 1
+ * len - next packet data
  */
-ZAPI int recv_packet(zsock_t sock, char *buf, int maxlen, int* offset, int *len, char bitmask, char *stuf);
+ZAPI int zrecv_packet(zsock_t sock, char *buf, int maxlen, int* offset, int *len, char bitmask);
+/**@fn int zconnectx(zsock_t sock, const char *host, uint 16_t port, int listenq)
+ * @brief listenq <= 0 active connect listenq > 0 passive connect
+ */
+ZAPI int zconnectx(zsock_t sock, const char *host, uint16_t port, int listenq);
 ZC_END
 #endif
